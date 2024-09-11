ca7bc06a9d94d2a17470af9901015f1d231ce3c6
aryan083 <aryanmahida2@gmail.com>
2024-09-11 22:05:03 +0530

    2220. Minimum Bit Flips to Convert Number

---
+++ 100 Days Challange/Day7.cpp
@@ -0,0 +1,14 @@
+// 2220. Minimum Bit Flips to Convert Number
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int result=0;
+        int num=start^goal;
+        while (num){
+            result+=num&1;
+            num=num>>1;
+           
+        }
+        return result;
+    }
+};
