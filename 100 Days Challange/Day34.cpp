eab1bf18a3d1406a2cacfb0e9503d77b318caa75
aryan083 <aryanmahida2@gmail.com>
2024-10-08 23:59:50 +0530

    Day 34: 1963. Minimum Number of Swaps to Make the String Balanced

---
+++ 100 Days Challange/Day34.cpp
@@ -0,0 +1,16 @@
+1963. Minimum Number of Swaps to Make the String Balanced
+class Solution {
+public:
+    int minSwaps(string s) {
+        int size = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '[')
+                size++;
+            else if (size > 0) 
+                size--;
+        }
+        return (size + 1) / 2;
+    }
+};
