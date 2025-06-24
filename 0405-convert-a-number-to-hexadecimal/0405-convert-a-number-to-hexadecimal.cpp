10840eab3389a7f0299ba9d843f81234998be988
aryan mahida <115215142+aryan083@users.noreply.github.com>
2025-06-24 21:33:52 +0530

    Time: 0 ms (100%), Space: 7.9 MB (45.84%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string toHex(int num) {
+        string ans  = "";
+        string map = "0123456789abcdef";
+        unsigned int num2 = num;
+        if(num == 0) return "0";
+        while( num2 ){
+            int rem =  num2 & 15;
+            ans = map[rem]+ans;
+            num2=num2>>4;
+        } 
+        return ans;
+    }
+};
