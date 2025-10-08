f34592dcd0f8ded65461596d2229aba0f5152d68
aryan mahida <115215142+aryan083@users.noreply.github.com>
2025-10-08 08:26:39 +0530

    Time: 438 ms (43.47%), Space: 187.1 MB (5.03%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,53 @@
+typedef vector<vector<char>> vvc;
+typedef set<pair<int, int>> spii;
+class Solution
+{
+public:
+    bool dfs(int row, int col, int i,
+         vvc &board, string &word, spii &paths)
+{
+    int ROWS = board.size(), COLS = board[0].size();
+    if (i == word.length())
+        return true;
+    if (row < 0 || col < 0 || row >= ROWS || col >= COLS 
+        || board[row][col] != word[i] || paths.count({row, col}))
+        return false;
+
+    paths.insert({row, col});
+
+    bool ans = (dfs(row + 1, col, i + 1, board, word, paths) ||
+                dfs(row - 1, col, i + 1, board, word, paths) ||
+                dfs(row, col + 1, i + 1, board, word, paths) ||
+                dfs(row, col - 1, i + 1, board, word, paths));
+
+    paths.erase({row, col});
+    return ans;
+}
+
+    bool exist(vector<vector<char>> &board, string word)
+    {
+        int ROWS = board.size(), COLS = board[0].size();
+
+        unordered_map<char, int> boardFreq, wordFreq;
+        for (auto &row : board)
+            for (char ch : row)
+                boardFreq[ch]++;
+        for (char ch : word)
+            wordFreq[ch]++;
+        for (auto &[ch, count] : wordFreq)
+            if (boardFreq[ch] < count) return false;
+
+        spii paths;
+        for (int r = 0; r < ROWS; r++)
+        {
+            for (int c = 0; c < COLS; c++)
+            {   
+                if(board[r][c]==word[0]){
+                if (dfs(r, c, 0, board, word, paths))
+                    return true;
+            }
+        }
+        }
+        return false;
+    }
+};
