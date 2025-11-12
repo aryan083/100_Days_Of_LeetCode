28694252e4ce80c7af50ef3e1d999c1acb5a96a9
aryan mahida <115215142+aryan083@users.noreply.github.com>
2025-11-12 12:21:49 +0530

    Time: 8 ms (27.27%), Space: 23.5 MB (97.65%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end()); 
+        int k = 0;        
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[k][1] >= intervals[i][0]) { // Overlap detected
+                intervals[k][1] = max(intervals[k][1], intervals[i][1]); // Merge
+            } else {
+                k++; // Move to the next position
+                intervals[k] = intervals[i]; // Replace in-place
+            }
+        }
+        
+        intervals.resize(k + 1); 
+        return intervals;
+    }
+};
