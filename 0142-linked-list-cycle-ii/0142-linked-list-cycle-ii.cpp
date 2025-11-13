d8864e087dd5b396552df9326956937731b7900a
aryan mahida <115215142+aryan083@users.noreply.github.com>
2025-11-13 11:51:18 +0530

    Time: 11 ms (16.37%), Space: 11.5 MB (21.42%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,6 +9,21 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        
+        //  if(head==NULL || head->next== NULL) return head;
+        ListNode* saslu =head;
+        ListNode* kachbo=head;
+        while(saslu !=NULL && saslu->next !=NULL){
+            kachbo=kachbo->next;
+            saslu =saslu->next->next;
+            if(saslu==kachbo){break;}
+        }
+        if(!saslu|| !saslu->next)return nullptr;
+        saslu=head;
+        while(saslu!=kachbo){
+            saslu=saslu->next;
+            kachbo=kachbo->next;
+        }
+        return kachbo;
+   
     }
 };
